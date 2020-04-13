000560b8601a8e934c16d9148b8489d65b9a0c47
Lucidastar <lucidahxx@gmail.com>
2020-04-13 23:31:23 +0800

    first commit

--- app/src/main/java/com/lucida/kotlinbase/base/unit1/Demo.kt
+++ app/src/main/java/com/lucida/kotlinbase/base/unit1/Demo.kt
@@ -1,4 +1,11 @@
 package com.lucida.kotlinbase.base.unit1
 
+import java.io.File
+import kotlin.math.round
+
 class Demo {
+    fun readText(){
+        readLine()
+
+    }
 }
--- app/src/main/java/com/lucida/kotlinbase/base/unit1/Test.kt
+++ app/src/main/java/com/lucida/kotlinbase/base/unit1/Test.kt
@@ -1,2 +1,8 @@
 package com.lucida.kotlinbase.base.unit1
 
+import kotlin.math.round
+
+fun main(array: Array<String>) {
+    println("hello")
+    println(round(12.0))
+}
